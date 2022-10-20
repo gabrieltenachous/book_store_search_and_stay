8af148e6fefe2a87604adf7547239fda3855b3c8
“Your <gabrielbromo@outlook.com>
2022-10-20 10:22:33 -0300

    Change nullable atributte BookStore and install doctrine/dbal in composer

---
+++ database/migrations/2022_10_20_131519_alter_book_store_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('book_stores', function (Blueprint $table) { 
+            $table->unsignedBigInteger('user_id')->nullable()->change(); 
+            $table->string('name')->nullable()->change();
+            $table->integer('isbn')->nullable()->change();
+            $table->decimal('value')->nullable()->change(); 
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('book_stores', function (Blueprint $table) { 
+            $table->unsignedBigInteger('user_id')->change(); 
+            $table->string('name')->change();
+            $table->integer('isbn')->change();
+            $table->decimal('value')->change(); 
+        });
+    }
+};
